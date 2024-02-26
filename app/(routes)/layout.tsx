6360a6d7f5331d61d2192debcfd681cd31576ceb
villivald <maxim.villivald@gmail.com>
2024-02-26 13:49:18 +0200

    review & fix css

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -11,11 +11,28 @@ export default function PageLayout({
 }) {
   const title = usePathname().substring(1).toUpperCase();
 
+  const colors = [
+    { green: ["brands", "tv", "indoor", "sharing", "social"] },
+    { teal: ["youtube", "discounts", "places", "caring"] },
+    { wine: ["magazines", "podcasts", "tour", "books"] },
+  ];
+
+  const pageColor = Object.keys(
+    colors.filter((color) =>
+      Object.values(color)[0].includes(title.toLowerCase())
+    )[0]
+  )[0];
+
   return (
     <div className={styles.layout}>
       <h1
         className={styles.title}
-        style={{ "--stringLength": title.length } as React.CSSProperties}
+        style={
+          {
+            "--stringLength": title.length,
+            "--pageColor": `var(--color-${pageColor})`,
+          } as React.CSSProperties
+        }
       >
         {title}
       </h1>
