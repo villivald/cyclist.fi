7b8c0235dbfe42cfd2d1d96cc974b3d218ef9911
villivald <maxim.villivald@gmail.com>
2024-02-26 19:47:40 +0200

    start building news block

--- components/homePage/News.tsx
+++ components/homePage/News.tsx
@@ -1,11 +1,42 @@
+import Link from "next/link";
+
 import styles from "@/styles/News.module.css";
 
 export default function News() {
   return (
     <section className={styles.newsContainer}>
       <h2>Last News</h2>
-      <article></article>
-      <article></article>
+      <article>
+        <section>
+          <p></p>
+          <p>News Content</p>
+        </section>
+        <section>
+          <p></p>
+          <p>News Content</p>
+        </section>
+        <section>
+          <p></p>
+          <p>News Content</p>
+        </section>
+      </article>
+      <article>
+        <section>
+          <p></p>
+          <p>News Content</p>
+        </section>
+        <section>
+          <p></p>
+          <p>News Content</p>
+        </section>
+        <section>
+          <p></p>
+          <p>News Content</p>
+        </section>
+      </article>
+      <span>
+        <Link href="/news">News archive</Link>
+      </span>
     </section>
   );
 }
