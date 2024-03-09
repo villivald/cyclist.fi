d1699d4944685f6793314dc24b6dd80038531975
villivald <maxim.villivald@gmail.com>
2024-03-09 12:52:50 +0200

    improve newslist on main page & contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useId } from "react";
 
 import styles from "@/styles/ContactForm.module.css";
 
@@ -8,6 +8,8 @@ export default function ContactForm() {
   const [email, setEmail] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [message, setMessage] = useState<string>("");
+
+  const id = useId();
 
   const submitButtonDisabled = !email || !name || !message;
 
@@ -23,37 +25,37 @@ export default function ContactForm() {
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="email">Your email (required)</label>
+        <label htmlFor={`email-${id}`}>Your email (required)</label>
         <input
           type="email"
           autoComplete="email"
           value={email}
           placeholder="mail@example.com"
           onChange={(e) => setEmail(e.target.value)}
-          id="email"
+          id={`email-${id}`}
           required
         />
       </div>
       <div>
-        <label htmlFor="name">Your name (required)</label>
+        <label htmlFor={`name-${id}`}>Your name (required)</label>
         <input
           type="text"
           autoComplete="name"
           value={name}
           placeholder="John Doe"
           onChange={(e) => setName(e.target.value)}
-          id="name"
+          id={`name-${id}`}
           required
         />
       </div>
       <div>
-        <label htmlFor="message">Your message (required)</label>
+        <label htmlFor={`message-${id}`}>Your message (required)</label>
         <textarea
           value={message}
           placeholder="Hello, I would like to talk about..."
           onChange={(e) => setMessage(e.target.value)}
-          id="message"
           rows={5}
+          id={`message-${id}`}
           required
         ></textarea>
       </div>
--- components/homePage/NewsList.tsx
+++ components/homePage/NewsList.tsx
@@ -1,51 +1,38 @@
 import Link from "next/link";
+import { useMemo } from "react";
 
 import NewsBlock from "./NewsBlock";
 
 import styles from "@/styles/NewsList.module.css";
 
-const placeholderText =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-const placeholderDate = "01.03.2024";
-const placeholderImage = "bg_xs.webp";
+import newsData from "./newsData.json";
 
 export default function NewsList() {
+  const firstNewsBlock = useMemo(() => newsData.slice(0, 3), []);
+  const secondNewsBlock = useMemo(() => newsData.slice(3, 6), []);
+
   return (
     <section className={styles.newsContainer}>
       <h2>Last News</h2>
       <article>
-        <NewsBlock
-          image={placeholderImage}
-          text={placeholderText}
-          date={placeholderDate}
-        />
-        <NewsBlock
-          image={placeholderImage}
-          text={placeholderText}
-          date={placeholderDate}
-        />
-        <NewsBlock
-          image={placeholderImage}
-          text={placeholderText}
-          date={placeholderDate}
-        />
+        {firstNewsBlock.map((news) => (
+          <NewsBlock
+            key={news.id}
+            image={news.image}
+            text={news.text}
+            date={news.date}
+          />
+        ))}
       </article>
       <article>
-        <NewsBlock
-          image={placeholderImage}
-          text={placeholderText}
-          date={placeholderDate}
-        />
-        <NewsBlock
-          image={placeholderImage}
-          text={placeholderText}
-          date={placeholderDate}
-        />
-        <NewsBlock
-          image={placeholderImage}
-          text={placeholderText}
-          date={placeholderDate}
-        />
+        {secondNewsBlock.map((news) => (
+          <NewsBlock
+            key={news.id}
+            image={news.image}
+            text={news.text}
+            date={news.date}
+          />
+        ))}
       </article>
       <span>
         <Link href="/news">News archive</Link>
