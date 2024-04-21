a48b7c278c086cd4fac74c989e4f4ffa25febf74
villivald <maxim.villivald@gmail.com>
2024-04-21 15:18:02 +0300

    enhance responsive version - news archive page #3

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,6 +3,8 @@ import { metadataElement } from "@/components/metadata";
 
 import newsData from "@/data/news.json";
 
+import styles from "@/styles/NewsArchiveBlock.module.css";
+
 export const metadata = metadataElement({
   title: "Uutiset",
 });
@@ -10,6 +12,7 @@ export const metadata = metadataElement({
 export default function news() {
   return (
     <div>
+      <h1 className={styles.title}>UUTISET</h1>
       {newsData.map((newsItem) => (
         <NewsArchiveBlock
           key={newsItem.id}
