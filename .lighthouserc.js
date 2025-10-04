78c0e30142ada64cf2fd8d0dbe1d4380e33e3b8f
MV <maxim.villivald@gmail.com>
2025-10-04 18:54:28 +0300

    add .lighthouserc.js

---
+++ .lighthouserc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npm run start',
+      url: ['http://localhost:3000'],
+      numberOfRuns: 1,
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
