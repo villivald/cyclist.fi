63c4db2f048aa61fc357dd621e5860fbfc7a8cd8
Maxim Villivald <maxim.villivald@gmail.com>
2025-10-06 13:45:34 +0300

    add vercel speed insights - #9

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale } from "next-intl/server";
@@ -74,6 +75,7 @@ export default async function RootLayout({
         </ThemeProvider>
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
