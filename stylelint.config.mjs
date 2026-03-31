a7a404d9e3871575c6fca37b248a49ec7b887019
Maxim Villivald <maxim.villivald@gmail.com>
2026-03-31 20:38:57 +0300

    add stylelint to ci

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,7 +2,6 @@
 const config = {
   extends: "stylelint-config-standard",
   rules: {
-    "block-no-empty": true,
     "selector-class-pattern": "(.*?)",
     "selector-id-pattern": "(.*?)",
     "custom-property-pattern": "(.*?)",
